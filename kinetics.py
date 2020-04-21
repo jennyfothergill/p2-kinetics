ba5d46ede05f8761761f0e3bc0b87c1cf1ce2b11
Jenny Fothergill <jennyfothergill@u.boisestate.edu>
2020-04-21 15:58:03 -0600

    blacked

--- kinetics.py
+++ kinetics.py
@@ -5,20 +5,25 @@ import numpy as np
 maxtime = 365
 
 # people in Idaho
-N = 1787065 # Idaho population estimate for July 1, 2019 from
-            # https://www.census.gov/quickfacts/ID
+N = 1787065  # Idaho population estimate for July 1, 2019 from
+# https://www.census.gov/quickfacts/ID
 
-def kinetics(k1, k2, S_0, I_0, R_0):   #Units of k1
+
+def kinetics(k1, k2, S_0, I_0, R_0):  # Units of k1
     def abc(t, y):
-        ''' System of differential equations: y(t) = [S(t),I(t),R(t)]
+        """ System of differential equations: y(t) = [S(t),I(t),R(t)]
             returns:
                 A list containing [dS/dt, dI/dt, dR/dt]
-        '''
+        """
         # Susceptible, Infected, Recovered
-        S,I,R = y
-        return [-k1*S*I/(S+I+R), k1*S*I/(S+I+R)-k2*I, k2*I]
-    return solve_ivp(
-            abc, [0, maxtime], [S_0,I_0,R_0],
-            t_eval=np.arange(0, maxtime, 1), method='Radau'
-            )
+        S, I, R = y
+        return [-k1 * S * I / (S + I + R), k1 * S * I / (S + I + R) - k2 * I, k2 * I]
 
+    return solve_ivp(
+        abc,
+        [0, maxtime],
+        [S_0, I_0, R_0],
+        t_eval=np.arange(0, maxtime, 1),
+        method="Radau",
+        dense_output=True,
+    )
--- plot-infections.py
+++ plot-infections.py
@@ -7,10 +7,10 @@ from kinetics import kinetics, N
 
 
 # For making the plot nicer
-plt.rcParams['figure.figsize'] = [9, 9]
-plt.rcParams['font.size'] = 18
-plt.rcParams['lines.linewidth'] = 4
-plt.rcParams['lines.markersize'] = 10
+plt.rcParams["figure.figsize"] = [9, 9]
+plt.rcParams["font.size"] = 18
+plt.rcParams["lines.linewidth"] = 4
+plt.rcParams["lines.markersize"] = 10
 
 
 # csv format is date,state,fips,cases,deaths
@@ -19,7 +19,7 @@ url = "https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-states.
 with requests.Session() as s:
     download = s.get(url)
 
-    decoded_content = download.content.decode('utf-8')
+    decoded_content = download.content.decode("utf-8")
 
     cr = csv.reader(decoded_content.splitlines(), delimiter=",")
     my_list = list(cr)
@@ -27,8 +27,8 @@ with requests.Session() as s:
 d_date = np.dtype([("date", np.unicode, 11), ("event", np.unicode, 256)])
 
 important_dates = np.loadtxt(
-        "data/important_dates.csv", skiprows=1, delimiter=", ", dtype=d_date
-        )
+    "data/important_dates.csv", skiprows=1, delimiter=", ", dtype=d_date
+)
 
 # idaho format is days_since_first_infection,date,cases,deaths,event
 idaho = []
@@ -38,7 +38,7 @@ for row in my_list:
     if row[1] == "Idaho":
         try:
             if row[0] == important_dates[event_i]["date"]:
-                #print(row[0])
+                # print(row[0])
                 event = important_dates[event_i]["event"]
                 event_i += 1
             else:
@@ -56,42 +56,42 @@ for row in my_list:
 
 # idaho format is days_since_first_infection,date,cases,deaths,event,weekend?
 idaho = np.array(idaho)
-weekend_inds = np.argwhere(idaho[:,5]==1)
-weekday_inds = np.argwhere(idaho[:,5]==0)
+weekend_inds = np.argwhere(idaho[:, 5] == 1)
+weekday_inds = np.argwhere(idaho[:, 5] == 0)
 
 
-plt.plot(idaho[weekend_inds,0],idaho[weekend_inds,2], "r.", label="infected")
-plt.plot(idaho[weekend_inds,0],idaho[weekend_inds,3], "k.", label="dead")
-plt.plot(idaho[weekday_inds,0],idaho[weekday_inds,2], "ro")
-plt.plot(idaho[weekday_inds,0],idaho[weekday_inds,3], "ko")
+plt.plot(idaho[weekend_inds, 0], idaho[weekend_inds, 2], "r.", label="infected")
+plt.plot(idaho[weekend_inds, 0], idaho[weekend_inds, 3], "k.", label="dead")
+plt.plot(idaho[weekday_inds, 0], idaho[weekday_inds, 2], "ro")
+plt.plot(idaho[weekday_inds, 0], idaho[weekday_inds, 3], "ko")
 
-event_inds = np.argwhere(idaho[:,4] != "")
-maxcase = max(idaho[:,2])
+event_inds = np.argwhere(idaho[:, 4] != "")
+maxcase = max(idaho[:, 2])
 for i in event_inds:
-    plt.plot((idaho[i,0],idaho[i,0]), (0,maxcase), "--", label=idaho[i,4][0])
+    plt.plot((idaho[i, 0], idaho[i, 0]), (0, maxcase), "--", label=idaho[i, 4][0])
 
 plt.legend(
-        title="small dots indicate weekend\nlarger dots are weekdays",
-        loc="upper left",
-        fontsize=14
-        )
-locs = list(idaho[:,0])[::5]
-labels = ["/".join(i.split("-")[1:]) for i in list(idaho[:,1])[::5]]
+    title="small dots indicate weekend\nlarger dots are weekdays",
+    loc="upper left",
+    fontsize=14,
+)
+locs = list(idaho[:, 0])[::5]
+labels = ["/".join(i.split("-")[1:]) for i in list(idaho[:, 1])[::5]]
 plt.xticks(locs, labels)
 plt.xlabel("date")
 plt.ylabel("people")
-plt.ylim((-10,maxcase*1.5))
+plt.ylim((-10, maxcase * 1.5))
 plt.show()
 
 
-model = kinetics(0.5,0.02, N-2,1,0)
+model = kinetics(0.5, 0.02, N - 2, 1, 0)
 
-plt.plot(idaho[:,0],idaho[:,2], "r.", label="infected")
-plt.plot(model.t, model.y[0], "-",  label='(model) susceptible')
-plt.plot(model.t, model.y[1], "--", label='(model) infected')
-plt.plot(model.t, model.y[2], ":", label='(model) recovered')
+plt.plot(idaho[:, 0], idaho[:, 2], "r.", label="infected")
+plt.plot(model.t, model.y[0], "-", label="(model) susceptible")
+plt.plot(model.t, model.y[1], "--", label="(model) infected")
+plt.plot(model.t, model.y[2], ":", label="(model) recovered")
 plt.xlabel("days")
 plt.ylabel("people")
 plt.legend()
-plt.ylim((-10,maxcase*1.5))
+plt.ylim((-10, maxcase * 1.5))
 plt.show()
