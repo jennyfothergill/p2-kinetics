72d4cd81b1ff2c69e63b91f38532b1ac62537164
Jenny Fothergill <jennyfothergill@u.boisestate.edu>
2020-04-22 10:09:02 -0600

    renamed file and started structure to loops over k1s/k2s and plot best values

--- plot_infections.py
+++ plot_infections.py
@@ -83,15 +83,30 @@ plt.ylabel("people")
 plt.ylim((-10, maxcase * 1.5))
 plt.show()
 
+def mse(y1,y2):
+    """Get the mean squared error between the y-value of two datasets
 
+    Parameters
+    ----------
+    y1, y2 : array_like
+        Input arrays to be compared
+
+    Returns
+    -------
+    float
+        Mean squared error between y1 and y2
+    """
+    return np.square(np.subtract(y1,y2))
+
+mses = []
+k1s = np.linspace(
+k2s =
 model = kinetics(0.5, 0.02, N - 2, 1, 0)
 
-plt.plot(idaho[:, 0], idaho[:, 2], "r.", label="infected")
-plt.plot(model.t, model.y[0], "-", label="(model) susceptible")
+plt.plot(idaho[:, 0], idaho[:, 2], "r.", label="(data) infected")
 plt.plot(model.t, model.y[1], "--", label="(model) infected")
-plt.plot(model.t, model.y[2], ":", label="(model) recovered")
 plt.xlabel("days")
 plt.ylabel("people")
-plt.legend()
+plt.legend(title=f"k1 = {k1} k2={k2}")
 plt.ylim((-10, maxcase * 1.5))
 plt.show()
