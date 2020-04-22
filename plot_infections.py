c3bcfd71ecd8c0c7d2e0a77eca6de47409cd4ac6
Jenny Fothergill <jennyfothergill@u.boisestate.edu>
2020-04-22 13:12:37 -0600

    print/plot model which seem to fit

--- plot_infections.py
+++ plot_infections.py
@@ -3,10 +3,10 @@ import numpy as np
 import requests
 import csv
 
-from kinetics import kinetics, N
+from kinetics import solve_sir, N
 
 
-def mse(y1,y2):
+def mse(y1, y2):
     """Get the mean squared error between the y-value of two datasets
 
     Parameters
@@ -19,7 +19,8 @@ def mse(y1,y2):
     float
         Mean squared error between y1 and y2
     """
-    return np.square(np.subtract(y1,y2))
+    return np.mean(np.square(np.subtract(y1, y2)))
+
 
 if __name__ == "__main__":
     # For making the plot nicer
@@ -27,7 +28,6 @@ if __name__ == "__main__":
     plt.rcParams["font.size"] = 18
     plt.rcParams["lines.linewidth"] = 4
     plt.rcParams["lines.markersize"] = 10
-
 
     # csv format is date,state,fips,cases,deaths
     url = "https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-states.csv"
@@ -75,7 +75,6 @@ if __name__ == "__main__":
     weekend_inds = np.argwhere(idaho[:, 5] == 1)
     weekday_inds = np.argwhere(idaho[:, 5] == 0)
 
-
     plt.plot(idaho[weekend_inds, 0], idaho[weekend_inds, 2], "r.", label="infected")
     plt.plot(idaho[weekend_inds, 0], idaho[weekend_inds, 3], "k.", label="dead")
     plt.plot(idaho[weekday_inds, 0], idaho[weekday_inds, 2], "ro")
@@ -99,16 +98,68 @@ if __name__ == "__main__":
     plt.ylim((-10, maxcase * 1.5))
     plt.show()
 
+    # mses = []
+    # k1_start = 0
+    # k1_stop = 1
+    # k2_start = 0
+    # k2_stop = 1
 
-    mses = []
-    #k1s = np.linspace(
-    #k2s =
-    model = kinetics(0.5, 0.02, N - 2, 1, 0)
+    # k1s = np.linspace(k1_start,k1_stop)
+    # k2s = np.linspace(k2_start,k2_stop)
+    # for k1 in k1s:
+    #    for k2 in k2s:
+    #        model = solve_sir(k1, k2, N - 2, 2, 0, maxday=max(idaho[:,0]))
+    #        mses.append((mse(model.y[1],idaho[:, 2]), k1, k2))
+
+    # mses = np.array(mses)
+    # mse_best = mses[:,0].min()
+    # best_row = np.argwhere(mses[:,0]==mse_best)[0][0]
+    # k1_best, k2_best = mses[best_row,1:]
+
+    # model = solve_sir(k1_best, k2_best, N - 2, 2, 0, maxday=max(idaho[:,0]))
+
+    k1_1 = 1.3
+    k2_1 = 1
+
+    model1 = solve_sir(k1_1, k2_1, N - 2, 2, 0)
+    # k1 = 1.3, k2 = 1 fits really well for beginning
+
+    switchday = 21  # this value was chosen simply by looking at the graph
+
+    k1_2 = 1.06
+    k2_2 = 1.025
+
+    model2 = solve_sir(k1_2, k2_2, N - idaho[switchday, 2], idaho[switchday, 2], 0)
+
+    print("Predicted maximum number of infected people:")
+    print(f"\tmodel 1: {max(model1.y[1]):.0f}")
+    print(f"\tmodel 2: {max(model2.y[1]):.0f}")
+
+    mse1 = mse(model1.y[1][:switchday], idaho[:switchday, 0])
+    maxday = idaho[-1, 0]
+    mse2 = mse(model2.y[1][switchday : maxday + 1], idaho[switchday:, 0])
+
+    print("\nMSE")
+    print(f"\tmodel 1: {mse1:.3f}\n\tmodel 2: {mse2:.3f}")
 
     plt.plot(idaho[:, 0], idaho[:, 2], "r.", label="(data) infected")
-    plt.plot(model.t, model.y[1], "--", label="(model) infected")
+
+    plt.plot(
+        model1.t[:switchday],
+        model1.y[1][:switchday],
+        "--",
+        label=f"(model) infected\nk1 = {k1_1} k2={k2_1}",
+    )
+
+    plt.plot(
+        model2.t + switchday,
+        model2.y[1],
+        "--",
+        label=f"(model) infected\nk1 = {k1_2} k2={k2_2}",
+    )
+
     plt.xlabel("days")
     plt.ylabel("people")
-    plt.legend(title=f"k1 = {k1} k2={k2}")
+    plt.legend()
     plt.ylim((-10, maxcase * 1.5))
     plt.show()
